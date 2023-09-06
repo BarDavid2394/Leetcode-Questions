268e9aa8e0bbf22fe10285f7fb902d8c5c416720
Bar David <130139786+BarDavid2394@users.noreply.github.com>
2023-09-06 23:43:52 +0300

    Time: 20 ms (78.17%), Space: 13.7 MB (69.62%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.cpp
+++ 0965-unique-email-addresses/0965-unique-email-addresses.cpp
@@ -2,32 +2,45 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         unordered_map<string, int> valid_mail;
-        int n = emails.size();
-        for(int i=0;i<n;i++){
-            string clean_mail;
-            string email = emails[i];
-            for(int j=0;j<email.length();j++){
-                if(email[j] == '@')
-                {
-                    clean_mail +=email.substr(j);
-                    if(!(valid_mail.find(clean_mail) != valid_mail.end())){
-                       valid_mail[clean_mail] = 1;
-                    }
-                    break; 
-                }
-                if(email[j] == '+'){
-                    while(j<email.length() && email[j] !='@'){
-                        j++;
-                    }
-                }
-                else if(email[j] != '.'){
-                    clean_mail +=email[j];
-                }
-                if(email[j] == '@')
-                    j--;
-                }
+        for(string &email : emails){
+          string clean_mail;
+          for(char c : email){
+            if(c == '@' || c == '+')
+                break;
+            if (c == '.')
+                continue;
+            clean_mail +=c;
+          }  
+        clean_mail += email.substr(email.find('@'));
+        valid_mail[clean_mail] = 1;
+
+        }
+        // int n = emails.size();
+        // for(int i=0;i<n;i++){
+        //     string clean_mail;
+        //     string email = emails[i];
+        //     for(int j=0;j<email.length();j++){
+        //         if(email[j] == '@')
+        //         {
+        //             clean_mail +=email.substr(j);
+        //             if(!(valid_mail.find(clean_mail) != valid_mail.end())){
+        //                valid_mail[clean_mail] = 1;
+        //             }
+        //             break; 
+        //         }
+        //         if(email[j] == '+'){
+        //             while(j<email.length() && email[j] !='@'){
+        //                 j++;
+        //             }
+        //         }
+        //         else if(email[j] != '.'){
+        //             clean_mail +=email[j];
+        //         }
+        //         if(email[j] == '@')
+        //             j--;
+        //         }
                 
-            }
+        //     }
         return valid_mail.size();   
     }
 };
