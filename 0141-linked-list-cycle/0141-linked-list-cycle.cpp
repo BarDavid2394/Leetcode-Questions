119d5b935acb1c3c4c4dd8abeadb8a9d144a645b
Bar David <130139786+BarDavid2394@users.noreply.github.com>
2023-09-09 15:07:05 +0300

    Time: 10 ms (47.59%), Space: 8.1 MB (75.9%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -7,38 +7,38 @@
  * };
  */
  //pointers approach
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//       ListNode *slow = head;
-//       ListNode *fast = head; 
-//       while (fast && slow && fast->next){
-//           slow = slow->next;
-//           fast = fast->next->next;
-//           if(fast == slow){
-//               return true;
-//           }
-//       }
-//       return false;
-//     }
-// };
-//hastable
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL)
-            return false;
-        unordered_map<ListNode*, int> map;
-        while(head != NULL){
-            if(map.count(head)>0)
-                return true;
-            else{
-                map[head] = 1;
-                head = head->next;
-            }
-        
-        }
-        return false;
+      ListNode *slow = head;
+      ListNode *fast = head; 
+      while (fast && slow && fast->next){
+          slow = slow->next;
+          fast = fast->next->next;
+          if(fast == slow){
+              return true;
+          }
+      }
+      return false;
     }
 };
+//hastable
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head == NULL)
+//             return false;
+//         unordered_map<ListNode*, int> map;
+//         while(head != NULL){
+//             if(map.count(head)>0)
+//                 return true;
+//             else{
+//                 map[head] = 1;
+//                 head = head->next;
+//             }
+        
+//         }
+//         return false;
+//     }
+// };
